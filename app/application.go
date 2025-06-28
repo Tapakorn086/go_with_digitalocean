7713233ac49db5c460cbb45390d64ec0b920548a
Tapakorn.ode <tapakorn.ode@gmail.com>
2025-06-28 15:21:11 +0700

    add: setup gin and health check

---
+++ app/application.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"learning/app/controllers"
+)
+
+type Application struct {
+	Server *controllers.Server
+}
+
+func New() *Application {
+	server := controllers.New()
+
+	return &Application{
+		Server: server,
+	}
+}
+
+func (app *Application) Run() {
+	app.Server.Run()
+}
